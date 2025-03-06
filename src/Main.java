ffec3f5eaa178ddc6e341955fbdc738a6b1d47de
BiskutBras <s72430@ocean.umt.edu.my>
2025-03-06 09:24:03 +0800

    Update Main.java
    now can receive user input for different character and height

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,25 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 
 		PyramidGenerator pyramid = new PyramidGenerator();
-		pyramid.generate(4);
-		System.out.println();
-		pyramid.generateReversed(4);
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Choose your character for pyramid: ");
+		char character = scanner.next().charAt(0);
+
+		System.out.println("Enter the pyramid height: ");
+		int height = scanner.nextInt();
+
+		scanner.close();
+
+		pyramid.generate(character, height);
+
+//		System.out.println(pyramid.getRow(3));
+//		System.out.println(pyramid.getSpace(4,3));
 	}
 
 }
